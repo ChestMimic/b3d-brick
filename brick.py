256bc9ec33c753de333745e5e5e9a4ac9a820800
Fitzgibbon <ibbolia@gmail.com>
2017-01-19 13:24:04 -0500

    Added (very basic) face calculation for brick class object

--- brick.py
+++ brick.py
@@ -3,7 +3,32 @@
 class Brick:
 	def __init__(self, length=4, width = 4, height = 2):
 		self.lwh = (length, width, height)
-		
+
+	def genMeshData(self):
+		verts = []
+		edges = []
+		faces = []
+
+		#Vertexes
+		verts.append((self.lwh[0], self.lwh[1], self.lwh[2]))	#0
+		verts.append((self.lwh[0], self.lwh[1], -self.lwh[2]))	#1
+		verts.append((self.lwh[0], -self.lwh[1], self.lwh[2]))	#2
+		verts.append((self.lwh[0], -self.lwh[1], -self.lwh[2]))	#3
+		verts.append((-self.lwh[0], self.lwh[1], self.lwh[2]))	#4
+		verts.append((-self.lwh[0], self.lwh[1], -self.lwh[2]))	#5
+		verts.append((-self.lwh[0], -self.lwh[1], self.lwh[2]))	#6
+		verts.append((-self.lwh[0], -self.lwh[1], -self.lwh[2]))#7
+
+		#Faces
+		faces.append((2,3,6,7)) #-Y
+		faces.append((0,1,4,5)) #+Y
+		faces.append((0,1,2,3)) #+X
+		faces.append((4,5,6,7))	#-X
+		faces.append((0,2,4,6))	#+Z
+		faces.append((1,3,5,7))	#-Z
+
+		return (verts, edges, faces)
+
 
 def getMin(points = []):
 	minX = 0
