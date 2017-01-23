1de1d7c725c99472ccc0fe9f025b340de8a6be0e
ibbolia <ibbolia@gmail.com>
2017-01-23 14:56:23 -0500

    Ability to add a brick item to blender eists, also some untested randomization.

--- brick.py
+++ brick.py
@@ -1,4 +1,4 @@
-import bpy
+import bpy, random
 
 class Brick:
 	def __init__(self, length=4, width = 4, height = 2):
@@ -20,15 +20,30 @@ class Brick:
 		verts.append((-self.lwh[0], -self.lwh[1], -self.lwh[2]))#7
 
 		#Faces
-		faces.append((2,3,6,7)) #-Y
-		faces.append((0,1,4,5)) #+Y
-		faces.append((0,1,2,3)) #+X
-		faces.append((4,5,6,7))	#-X
-		faces.append((0,2,4,6))	#+Z
-		faces.append((1,3,5,7))	#-Z
+		#Blender determines normals by vertices in a face clockwise
+		faces.append((2,3,7,6)) #-Y (Front)
+		faces.append((0,4,5,1)) #+Y (Back)
+		faces.append((0,1,3,2)) #+X (Right)
+		faces.append((5,4,6,7))	#-X (Left)
+		faces.append((0,2,6,4))	#+Z (Top)
+		faces.append((1,5,7,3))	#-Z (Bottom)
 
-		return (verts, edges, faces)
+		self.verts = verts
+		self.edges = edges
+		self.faces = faces
 
+	def randomizeVerts(self, min = -.5, max = .5):
+		verts = []
+
+		for vtx in self.verts:
+			tup = []
+			for xyz in vtx:
+				rnd = random.uniform(min, max)
+				tup.append(xyz + rnd)
+			verts.append((tup[0], tup[1], tup[2]))
+			
+
+		pass
 
 def getMin(points = []):
 	minX = 0
@@ -49,7 +64,27 @@ def getMax(points= []):
 	return {'X':minX, 'Y':minY}
 
 class BrickGeneratorOperator(bpy.types.Operator):
-	pass
+	bl_idname = "object.brick"
+	bl_label = "Brick"
+	bl_options = {'REGISTER', 'UNDO'}
+
+	def execute(self, context):
+		#Create mesh data
+		mesh_data = bpy.data.meshes.new("brick_mesh_data")
+		samp_brick = Brick()
+		samp_brick.genMeshData()
+		mesh_data.from_pydata(samp_brick.verts, samp_brick.edges, samp_brick.faces)
+		mesh_data.update()
+
+		#create blender object
+		obj = bpy.data.objects.new("Brick", mesh_data)
+		obj.location = bpy.context.scene.cursor_location
+
+		#Link object to mesh data
+		scene = bpy.context.scene
+		scene.objects.link(obj)
+
+		return {'FINISHED'}
 
 if __name__ == "__main__":
 	points = [
